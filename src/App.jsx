974ff107dd8fcc213a916277f1902b135d34ba53
LucasTool <lucas.toulet@gmail.com>
2022-11-24 19:50:27 +0100

    upload creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import React from "react";
+import UploadAndDisplayImage from "./components/upload";
+
 function App() {
-  return <div className="font-fredoka">Hello</div>;
+  return (
+    <div className="font-fredoka">
+      <UploadAndDisplayImage />
+    </div>
+  );
 }
 
 export default App;
