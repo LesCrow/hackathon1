207d5cc857739bf5211a44f82156d61c98a6bd90
Damsbot64 <damien64durand@gmail.com>
2022-11-24 19:19:04 +0100

    feature works

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { useState, createRef } from "react";
 import * as htmlToImage from "html-to-image";
 
 const buttons = [
-  { id: 1, value: "petite", class: "w-20" },
-  { id: 2, value: "moyenne", class: "w-32 h-44" },
-  { id: 3, value: "grande", class: "w-60" },
+  { id: 1, value: "petite", class: "w-20 absolute" },
+  { id: 2, value: "moyenne", class: "w-32  absolute" },
+  { id: 3, value: "grande", class: "w-60 absolute" },
 ];
 
 const createFileName = (extension = "", ...names) => {
@@ -35,35 +35,34 @@ function App() {
   const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
 
   return (
-    <div ref={ref} className="flex flex-col ">
-      <div className="flex flex-col ">
-        <h1 className="w-full flex justify-center">
-          Image recuperé par l'api avec le background en moins
-        </h1>
-        <div className="flex flex-row w-full justify-center">
-          {buttons.map((button, id) => (
-            <button
-              key={id}
-              onClick={() => setSelectedSize(button)}
-              className="rounded bg-gray-400  p-1 m-2 h-12"
-            >
-              {button.value}
-            </button>
-          ))}
-        </div>{" "}
-        <div className="flex justify-center pb-2">
-          <motion.img
-            drag
-            className={`${selectedSize.class}`}
-            src="/Photo_Thomas_pour_test.png"
-          />
-        </div>
-        <div className="flex flex-col justify-around"></div>
-      </div>
-      <h2>nouveau background pour l'image precedente</h2>
+    <div>
       <div>
-        <img src={beach} className="w-full h-[600px] object-fill" />
-        <button onClick={downloadScreenshot}>Download screenshot</button>
+        {buttons.map((button, id) => (
+          <button
+            key={id}
+            onClick={() => setSelectedSize(button)}
+            className="rounded bg-gray-400  p-1 m-2 h-12"
+          >
+            {button.value}
+          </button>
+        ))}
+      </div>
+      <div ref={ref} className="flex flex-col ">
+        <div className="flex flex-col  ">
+          <div className="flex flex-row w-full justify-center"></div>{" "}
+          <div className="flex justify-center pb-2">
+            <motion.img
+              drag
+              className={`${selectedSize.class}`}
+              src="/Photo_Thomas_pour_test.png"
+            />
+          </div>
+          <div className="flex flex-col justify-around"></div>
+        </div>
+        <div>
+          <img src={beach} className="w-full h-[600px] object-fill" />
+          <button onClick={downloadScreenshot}>Download screenshot</button>
+        </div>
       </div>
     </div>
   );
