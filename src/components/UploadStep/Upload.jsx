226277c155eb912e5f202c967f5a6bcffb70b08e
LesCrow <john.baruteau@gmail.com>
2022-11-25 12:58:09 +0100

    feature is working

--- src/components/UploadStep/Upload.jsx
+++ src/components/UploadStep/Upload.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
+import Step3 from "../EndDownload/Step3";
+
 import Bodies1bold from "../Shared/Bodies1bold";
 import ButtonCTA from "../Shared/ButtonCTA";
 import Titles from "../Shared/Titles";
@@ -19,7 +21,7 @@ export function getImage(file) {
   });
 }
 
-const UploadAndDisplayImage = () => {
+const UploadAndDisplayImage = ({ isStep2, isStep3 }) => {
   const [yourImage, setImage] = useState([]);
   const [isUploaded, setIsUploaded] = useState(false);
   const [responseImage, setResponseImage] = useState(null);
@@ -63,49 +65,61 @@ const UploadAndDisplayImage = () => {
 
   return (
     <div>
-      {responseImage && <img src={responseImage} />}
-      <Titles greyTitle={"SELECT YOUR"} purpleTitle={"TRIPR"} />
-      <Bodies1bold
-        greyBody={"Choose the"}
-        purpleBody={"person"}
-        greyBodyNext={"you want in your paradise, maybe you ?"}
-      />
-      <div
-        className={`${
-          !isUploaded &&
-          "h-[350px] w-[70%] mx-auto flex flex-col border-dashed border-4 rounded-[50px] border-gray-300 bg-white justify-center items-center"
-        } w-full h-4/5`}
-      >
-        <ButtonCTA
-          cta={"UPLOAD"}
-          func={() => removeBackgroundApiCall(acceptedFiles[0])}
-        />
-        <div className="mt-5" {...getRootProps()}>
-          <input {...getInputProps()} />
-          {isDragActive ? (
-            <div className="h-4/5 w-4/5">Drop the Image here</div>
-          ) : (
-            <div className=" font-nunito text-3xl">
-              or <span className="font-bold text-purpleText">drag & drop.</span>
-            </div>
-          )}
-        </div>
-        <p className="text-sm text-center mt-4">
-          Format <span className="font-bold mx-1">jpg, png</span> ou
-          <span className="font-bold mx-1">webp</span>.<br />
-          Max
-          <span className="font-bold mx-1">30 Mo</span>.
-        </p>
+      {isStep2 && (
         <div>
-          {yourImage.map((upFile) => {
-            return (
-              <div>
-                <img src={upFile.preview} className="w-[80%]" alt="preview" />
-              </div>
-            );
-          })}
+          {responseImage && <img src={responseImage} />}
+          <Titles greyTitle={"SELECT YOUR"} purpleTitle={"TRIPR"} />
+          <Bodies1bold
+            greyBody={"Choose the"}
+            purpleBody={"person"}
+            greyBodyNext={"you want in your paradise, maybe you ?"}
+          />
+          <div
+            className={`${
+              !isUploaded &&
+              "h-[350px] w-[70%] mx-auto flex flex-col border-dashed border-4 rounded-[50px] border-gray-300 bg-white justify-center items-center"
+            } w-full h-4/5`}
+          >
+            <ButtonCTA
+              cta={"UPLOAD"}
+              func={() => removeBackgroundApiCall(acceptedFiles[0])}
+            />
+            <div className="mt-5" {...getRootProps()}>
+              <input {...getInputProps()} />
+              {isDragActive ? (
+                <div className="h-4/5 w-4/5">Drop the Image here</div>
+              ) : (
+                <div className=" font-nunito text-3xl">
+                  or{" "}
+                  <span className="font-bold text-purpleText">
+                    drag & drop.
+                  </span>
+                </div>
+              )}
+            </div>
+            <p className="text-sm text-center mt-4">
+              Format <span className="font-bold mx-1">jpg, png</span> ou
+              <span className="font-bold mx-1">webp</span>.<br />
+              Max
+              <span className="font-bold mx-1">30 Mo</span>.
+            </p>
+            <div>
+              {yourImage.map((upFile) => {
+                return (
+                  <div>
+                    <img
+                      src={upFile.preview}
+                      className="w-[80%]"
+                      alt="preview"
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-      </div>
+      )}
+      {isStep3 && <Step3 responseImage={responseImage} />}
     </div>
   );
 };
